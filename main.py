35d8511d23a2cabcb09c1cdbf2f43c34785824ff
s27113 <22258235+s27113@users.noreply.github.com>
2021-11-14 19:26:29 -0500

    add streamlit

---
+++ main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from top2vec import Top2Vec as T2V
+#from sklearn.datasets import fetch_20newsgroups as news
+
+import matplotlib.pyplot as plt
+
+
+st.title('CS410 Project: Topic Modeling')
+
+avail_data = ['arxiv', 'twitter', 'cbc', 'reddit', 'sklearn20news']
+dataset = st.sidebar.selectbox('dataset', avail_data, index=4, help='Choose dataset to perform topic modeling')
+
+avail_algo = ['top2vec', 'LDA']
+algo = st.sidebar.selectbox('algorithm', avail_algo, help='Choose algorithm to perform topic modeling')
+
+
+if dataset=='arxiv':
+    date = st.sidebar.date_input('date')
+    start = st.sidebar.time_input('time')
+
+@st.cache
+def generate_model():
+    #newsgroups = news(subset='all', remove=('headers', 'footers', 'quotes'))
+    #model = T2V(documents=newsgroups.data, speed="fast-learn", workers=8)
+    model = T2V.load('20news.model')
+    return model
+
+model = generate_model()
+_n = model.get_num_topics()
+ntopic = st.sidebar.number_input('number of topics', 1, 999, _n, 
+                                 help=f'{_n} is the recommended number determined by the algorithm, but you are free to modify it to see how it changes the topic model') 
+
+model.generate_topic_wordcloud(1)
+plt.savefig('data/temp.png')
+st.image('data/temp.png', caption='wordcloud for topic 1')
