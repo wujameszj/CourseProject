f24e2f338f90dedf1c735b3443e4ce962daeb2bf
s27113 <22258235+s27113@users.noreply.github.com>
2021-12-02 20:53:14 -0500

    use expander to show docs

--- main.py
+++ main.py
@@ -75,6 +75,7 @@ def generate_t2v_model(data, speed='learn'):
 def retrieve(dataset, fromDate=None, toDate=None):    
     if dataset == 'sklearn20news':
         return news(subset='all', remove=('headers', 'footers', 'quotes')).data
+    
    
 @st.experimental_memo
 def _create_wordcloud(word_prob):
@@ -95,6 +96,12 @@ def create_wordcloud(model, topicIDs, nWords=30):
         wc_list.append(_create_wordcloud(word_prob))
         # canvas.image(wc_list)
     return wc_list
+
+@st.experimental_memo(suppress_st_warning=True)
+def display_doc(docs):
+    for i, doc in enumerate(docs):
+        with st.expander(f'Doc {i+1}', True):
+            st.text(doc)
     
     
 def main():
@@ -108,6 +115,7 @@ def main():
 
     avail_data = ['arxiv', 'twitter', 'NYU/nips12raw_str602', 'reddit', 'sklearn20news']
     with st.sidebar:
+        st.subheader('Step 1:')
         dataset = st.selectbox('dataset', avail_data, index=4, help='Choose dataset to perform topic modeling')
         if dataset == 'arxiv':
             fromdate = st.date_input('from date')
@@ -119,54 +127,58 @@ def main():
     t2v_model = generate_t2v_model(data)
 
     nTopic = t2v_model.get_num_topics()
-    DEFAULT_WORDCLOUD = 5
-    nExample = DEFAULT_WORDCLOUD if DEFAULT_WORDCLOUD < nTopic else nTopic 
+    DEFAULT_EXAMPLE = 3
+    nExample = DEFAULT_EXAMPLE if DEFAULT_EXAMPLE < nTopic else nTopic 
     
     topics, _, __ = t2v_model.get_topics(nTopic)
     topic_words = [None] + [words[0] for words in topics] 
 
     with st.sidebar:
-        st.subheader('LDA parameters')
+        st.subheader('Step 2: LDA parameters')
         nTopic = int(st.number_input('number of topics', 0, 999, 0, help=f'A larger number increases computation time. Based on Top2vec, we recommend {int(nTopic/1.5)} for this dataset.'))
         optional = st.expander('optional training parameters')
         with optional:    
             passes = int(st.number_input('passes', 1, 99, 2, help='Higher number increases model quality at the cost of computation time.'))
             iters = int(st.number_input('iterations', 1, 999, 50, help='Higher number increases model quality at the cost of computation time.'))
-        topic = st.selectbox('search topic by word', topic_words, help='This list consists of likely topic words in this dataset.')
+        st.subheader('Step 3: Compare topics and documents')
+        topic = st.selectbox('search by keyword', topic_words, help='This list consists of likely topic words in this dataset.')
 
     
     with left:
         if topic is None:
             msg.info(f'Displaying {nExample} unrelated topics and documents.')
-            topicIDs, docIDs = range(nExample), range(nExample)
+            topicIDs, docIDs = range(nExample), range(nExample*2)
         else:
             msg.info(f'Displaying {nExample} topics and documents related to "{topic}".')
             _,_,_, topicIDs = t2v_model.query_topics(str(topic), nExample)
-            _,_, docIDs = t2v_model.search_documents_by_keywords([topic], nExample, keywords_neg=None, return_documents=True, use_index=False, ef=len(data['data'])  # ef max value for best search accuracy
+            _, docIDs = t2v_model.search_documents_by_keywords([topic], nExample*2, keywords_neg=None, return_documents=False, use_index=False, ef=len(data['data']))
         st.image(create_wordcloud(t2v_model, topicIDs))
-        st.table([data['data'][i] for i in docIDs])
-    
+#        st.table([data['data'][i] for i in docIDs])
+        display_doc( [data['data'][i] for i in docIDs] )
+        
     
     if nTopic:
         with right:
            # patient.info(f'Training model with {nTopic} topics for {passes} passes and {iters} iterations ... could take more than {nTopic*passes*iters//99} minutes. Please be patient.')
-            patient = st.info(f'Training LDA model with {nTopic} topics for {passes} passes and {iters} iterations. Please be patient.')
+            patient = st.info(f'Training with {nTopic} topics for {passes} passes and {iters} iterations. Please be patient.')
 #            lda_model, dictionary, doc_topic_prob = generate_LDA_model(data, nTopic, passes, iters)
             lda_model, dictionary, corpus = generate_LDA_model(data, nTopic, passes, iters)
            
             if topic is None:
-                topicIDs, docIDs = range(nExample), range(nExample, nExample*2)
+                topicIDs, docIDs = range(nExample), range(nExample*2, nExample*4)
             else:
                 topicIDs = lda_model.get_term_topics(dictionary.index(topic), minimum_probability=0)
                 topicIDs = [i for i,_ in topicIDs[:nExample]]
                 
  #               doc_prob = calc_relevance(doc_topic_prob, topicIDs[0])
                 doc_prob = calc_relevance(corpus, dictionary.index(topic))
-                docIDs = argp(doc_prob, -nExample)[-nExample:]
+                docIDs = argp(doc_prob, -nExample*2)[-nExample*2:]
                 docIDs = docIDs[ argsort(doc_prob[docIDs])[::-1] ]    # list largest first
                 
             st.image(create_wordcloud(lda_model, topicIDs))
-            st.table([data['data'][i] for i in docIDs])
+#            st.table([data['data'][i] for i in docIDs])
+            display_doc( [data['data'][i] for i in docIDs] )
+#            st.table( st.expander(data['data'][i]) for i in docIDs )
 
 
     patient.empty()
